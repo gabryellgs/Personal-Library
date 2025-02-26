4223a78987733ea1dfaef45301d288df2d10a534
Suanderson <Suanderson.santos@outlool.com>
2025-02-26 10:47:22 -0300

    organização das urls

--- BibliotecaPessoal/urls.py
+++ BibliotecaPessoal/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from core.views import index
+from core.views import index, telaP
 
 urlpatterns = [
     path('index/',index),
+    path('telaP/',telaP ),
     path('admin/', admin.site.urls),
 ]
